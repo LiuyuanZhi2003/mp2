17caf519737163949472e815dbb678a446046507
LiuyuanZhi2003 <lzhi2@illinois.edu>
2025-10-07 18:58:40 -0500

    my mp2

--- src/pages/DetailView.tsx
+++ src/pages/DetailView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { api, FIELDS, getCached } from '../api/aic';
 import { ArtItem } from '../types';
 import { ListContext } from '../context/ListContext';
